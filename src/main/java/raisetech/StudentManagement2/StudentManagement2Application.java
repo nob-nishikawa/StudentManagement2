6248aff17bf096ca63447d47f27310a0b737fd5e
Nobuki Nishikawa <n_nishikawa1976@yahoo.co.jp>
2025-07-02 17:31:14 +0900

    Initial commit

---
+++ src/main/java/raisetech/StudentManagement2/StudentManagement2Application.java
@@ -0,0 +1,13 @@
+package raisetech.StudentManagement2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagement2Application.class, args);
+	}
+
+}
