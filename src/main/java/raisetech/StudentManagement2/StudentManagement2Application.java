a0d99706b781fefcb5cf93077fc0154a42386fec
Nobuki Nishikawa <n_nishikawa1976@yahoo.co.jp>
2025-07-05 12:07:02 +0900

    演習課題の追加 Map<>を使用したものを追加

--- src/main/java/raisetech/StudentManagement2/StudentManagement2Application.java
+++ src/main/java/raisetech/StudentManagement2/StudentManagement2Application.java
@@ -1,9 +1,13 @@
 package raisetech.StudentManagement2;
 
+import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,8 +16,19 @@ public class StudentManagement2Application {
 
   private String studentName = "山田";
   private String age = "18";
+  //  private Map<String, String> studentInformation = Map.of("田中", "20");
+  private final Map<String, String> studentInfomation = new HashMap<>();
+
+  //  初期値をセット
+  @PostConstruct
+  public void setMappingData() {
+    studentInfomation.put("田中", "30");
+    studentInfomation.put("石破", "60");
+
+  }
 
   public static void main(String[] args) {
+
     SpringApplication.run(StudentManagement2Application.class, args);
   }
 
@@ -25,6 +40,11 @@ public class StudentManagement2Application {
   @GetMapping("/studentInfo")
   public String studentInfo() {
     return studentName + " " + age + "歳";
+  }
+
+  @GetMapping("/studentInfo2")
+  public Map<String, String> studentInfo2() {
+    return studentInfomation;
   }
 
   @PostMapping("/updateStudentInfo")
@@ -43,5 +63,19 @@ public class StudentManagement2Application {
   @PostMapping("/updateStudentAge")
   public void updateStudentAge(String age) {
     this.age = age;
+  }
+
+  @PostMapping("/studentInfo2")
+  public void studentInfo2(@RequestParam Map<String, String> updateParam) {
+    String getName = updateParam.get("studentName");
+    String getAge = updateParam.get("age");
+
+    if (getName != null && getAge != null) {
+
+      studentInfomation.put(getName, getAge);
+    } else {
+      System.err.println("パラメータがありませんでした");
+    }
+
   }
 }
